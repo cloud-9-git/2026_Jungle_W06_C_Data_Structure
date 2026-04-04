9ce28e9b249e12ce7c7fb454debd9df0c19b1a73
cloud-9-git <reum.celeste.koo@gmail.com>
2026-04-04 16:29:40 +0900

    Linked List: Q2_A_LL alternateMergeLinkedList 코드 구현 및 코드 리뷰

--- Data-Structures/Linked_List/Q2_A_LL.c
+++ Data-Structures/Linked_List/Q2_A_LL.c
@@ -104,7 +104,91 @@ int main()
 void alternateMergeLinkedList(LinkedList *ll1, LinkedList *ll2)
 {
     /* add your code here */
+	if (ll1->head == NULL || ll2->head == NULL) {
+		return;
+	}
+	ListNode *curOne = ll1->head;
+	ListNode *curTwo = ll2->head;
+
+	while (curOne != NULL || curTwo != NULL) {
+			// ll2에서 가져 올 노드와 ll2의 분리. 다음 노드를 가지고 있는지 없는지에 따라 ll2 head의 값이 바뀐다
+			if (curTwo->next == NULL) {
+				ll2->head = NULL;
+				ll2->size--;
+			} else {
+				ll2->head = curTwo->next;
+				ll2->size--;
+			}
+			// ll1에서 현재 가리키고 있는 노드와 그 노드의 다음 노드 사이에 ll2에서 가져 온 노드를 끼워 넣는다.
+			// ll1에 사이공간이 더 있는지 확인한다.
+			if (curOne->next == NULL) {
+				curOne->next = curTwo;
+				curTwo->next = NULL;
+				ll1->size++;
+				break;	
+			} 
+			else {
+				curTwo->next = curOne->next;
+				curOne->next = curTwo;
+				ll1->size++;	
+				// curOne과 curTwo 업데이트
+				//// 더 끼워넣을 노드가 남아 있는지 확인한다.
+				if (ll2->head == NULL) {
+					break;
+				//// 더 끼워넣을 노드가 남아 있다면 포인터의 위치를 업데이트 한다.
+				} else {
+					curOne = curTwo->next;
+					curTwo = ll2->head;
+				}
+			}
+
+	}
+	return;
 }
+
+/*
+// 제미나이에게 코드 리뷰 부탁했을 때 써 준 코드
+void alternateMergeLinkedList(LinkedList *ll1, LinkedList *ll2)
+{
+    // 한쪽이라도 비어있으면 작업할 게 없음
+    if (ll1->head == NULL || ll2->head == NULL) {
+        return;
+    }
+
+    ListNode *cur1 = ll1->head;
+    ListNode *cur2 = ll2->head;
+    ListNode *next1, *next2;
+
+    // 두 리스트 모두 노드가 있을 때까지만 반복
+    while (cur1 != NULL && cur2 != NULL) {
+        // 1. 다음 노드들 미리 백업 (길을 잃지 않기 위해)
+        next1 = cur1->next;
+        next2 = cur2->next;
+
+        // 2. cur1 뒤에 cur2를 연결
+        cur1->next = cur2;
+        ll1->size++;
+        ll2->size--;
+
+        // 3. cur2의 다음을 어떻게 처리할지 결정
+        if (next1 == NULL) {
+            // ll1이 여기서 끝난다면, cur2를 ll1의 진짜 마지막으로 만듦
+            cur2->next = NULL;
+            ll2->head = next2; // ll2의 새로운 시작점 설정
+            break;
+        } else {
+            // ll1에 노드가 더 있다면, cur2 뒤에 원래 ll1의 다음 노드를 연결
+            cur2->next = next1;
+			ll2->head = next2; // ll2의 새로운 시작점 설정
+        }
+
+        // 4. 다음 쌍으로 이동
+        cur1 = next1;
+        cur2 = next2;
+    }
+}
+*/
+
 
 ///////////////////////////////////////////////////////////////////////////////////
 
